38e68297251705c423337ea2ebc4a25454d16f9c
betashort <31877309+betashort@users.noreply.github.com>
2018-01-15 02:17:38 +0900

    f

--- kadai7.m
+++ kadai7.m
@@ -1,18 +1,20 @@
-% 課題7　ダイナミックレンジの拡大
-% 画素のダイナミックレンジを０から２５５にせよ．
-% 下記はプログラムである．
+% 課�?7�?�?イナミ�?クレンジの拡大
+% 画�?の�?イナミ�?クレンジを０から２５５にせよ?�?
+% 下記�?�プログラ�?である?�?
 
-ORG = imread('AMEMAN17826009_TP_V4.jpg'); % 画像の入力
-ORG = rgb2gray(ORG); % 白黒濃淡画像に変換
-imagesc(ORG); colormap(gray); colorbar; % 画像の表示
+ORG = imread('AMEMAN17826009_TP_V4.jpg'); % 画像�?�入�?
+ORG = rgb2gray(ORG); % 白黒�?淡画像に変換
+imagesc(ORG); colormap(gray); colorbar; % 画像�?�表示
 pause;
-imhist(ORG); % 濃度ヒストグラムを生成、表示
+imhist(ORG); % �?度ヒストグラ�?を生成�?�表示
 pause;
 ORG = double(ORG);
-mn = min(ORG(:)); % 濃度値の最小値を算出
-mx = max(ORG(:)); % 濃度値の最大値を算出
+mn = min(ORG(:)); % �?度値の�?小�?�を算�?�
+mx = max(ORG(:)); % �?度値の�?大値を算�?�
 ORG = (ORG-mn)/(mx-mn)*255;
-imagesc(ORG); colormap(gray); colorbar; % 画像の表示
+imagesc(ORG); colormap(gray); colorbar; % 画像�?�表示
+pause;
+imhist(ORG);
 pause;
 ORG = uint8(ORG); 
 imhist(ORG);
